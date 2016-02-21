aafa9bc3d24cf3ca1f35fe4f3638088b6b5bf1a8
Fernanda <nandafercer@gmail.com>
2016-02-21 12:14:32 -0300

    Primeiro commit

---
+++ master/ProjetoClientes/README.md
@@ -0,0 +1,4 @@
+ProjetoClientes
+===============
+
+A Symfony project created on February 21, 2016, 11:13 am.
---
+++ master/ProjetoClientes/src/AppBundle/AppBundle.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace AppBundle;
+
+//use Symfony\Component\HttpKernel\Bundle\Bundle;
+//use C:\Apache\Apache24\htdocs\ProjetoClientes\vendor\symfony\symfony\src\Symfony\Component\HttpKernel\Bundle;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AppBundle extends Bundle
+{
+	private $clientes;
+	
+	public function Cadastro() {
+		//$cadastro = array();
+		
+		$array = array( 'matricula' -> $matricula,
+							   'nome' -> $nome,
+							   'dtnascimento' -> $dtnascimento,
+							   'endereco' -> $endereco,
+							   'email' -> $email,
+							   'telefone' -> $telefone,
+							   'celular' -> $celular,
+							   'login' -> $login,
+							   'senha' -> $senha );
+
+	}
+	
+	/*public function Incluir(){
+		 $this->cadastro[] = $incluirCliente;
+	}
+	
+	public function todosClientes(){
+        foreach($this->cadastro as $obj){
+            echo "Matrícula: {$obj->getMatricula() } <br/>
+				  Nome: {$obj->getNome()}<br/>
+                  Telefone: {$obj->getTelefone()}<br/>
+                  Email: {$obj->getEmail()}<br/>
+                  -----------------<br/>";
+        }
+    }
+	*/
+		
+	/*function __construct() {
+       $this->matricula = $matricula;
+	   $this->nome = $nome;
+	   $this->dtnascimento = $dtnascimento;
+	   $this->endereco = $endereco;
+	   $this->email = $email;
+	   $this->telefone = $telefone;
+	   $this->celular = $celular;
+	   $this->login = $login;
+	   $this->senha = $senha;
+	}*/
+	    
+}
+
+$cliente = new AppBundle();
+?>
+<html>
+<body>
+
+<form>
+	Matrícula: 	<input name='txtMatricula'>
+</form>
+
+</body>
+</html>
